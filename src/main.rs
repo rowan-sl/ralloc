7973aa07b7bfbd4392649bb68fa4564e1065b402
Rowan S-L <rowan.with.cats@gmail.com>
2022-08-31 11:08:05 -0400

    global alloc is now safe

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 #![feature(once_cell)]
 
 use numtoa::NumToA;
-use ralloc::{allocator::RAlloc};
+use ralloc::allocator::RAlloc;
 use na_print::{eprintln, print, println};
-use std::{cell::UnsafeCell, alloc::GlobalAlloc, process, ptr::{NonNull, addr_of_mut}};
+use core::{cell::RefCell, alloc::GlobalAlloc, ptr::NonNull};
+use std::{process, sync::{LazyLock, Mutex}};
 
 // // panic hook for debugging things (does not work)
 // static DEFAULT_HOOK: LazyLock<Box<dyn Fn(&panic::PanicInfo<'_>) + Sync + Send + 'static>> =
@@ -40,32 +41,44 @@ use std::{cell::UnsafeCell, alloc::GlobalAlloc, process, ptr::{NonNull, addr_of_
 
 const DO_DEBUG_PRINTS: bool = false;
 
-static mut IN_ALLOC: bool = false;
-static mut MEMORY: [u8; 5_000] = [0u8; 5_000];
-#[global_allocator]
-static GLOBAL_ALLOC: GlobalAllocator = GlobalAllocator::new();
+thread_local! {
+    /// recursive allocation detection (must be thread local to remove false positives with multithreading)
+    static IN_ALLOC: RefCell<bool> = RefCell::new(false);
+}
 
-struct GlobalAllocator(UnsafeCell<RawGlobalAllocator>);
-unsafe impl Sync for GlobalAllocator {}
-impl GlobalAllocator {
+#[global_allocator]
+static GLOBAL_ALLOC: GlobalRalloc = GlobalRalloc::new();
+
+pub struct GlobalRalloc(LazyLock<Mutex<RawGlobalRalloc>>);
+
+impl GlobalRalloc {
     pub const fn new() -> Self {
-        Self(UnsafeCell::new(RawGlobalAllocator::new()))
+        Self(LazyLock::new(|| {
+            let v = Mutex::new(RawGlobalRalloc::new());
+            v
+        }))
     }
 }
-unsafe impl GlobalAlloc for GlobalAllocator {
+
+unsafe impl GlobalAlloc for GlobalRalloc {
     unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
-        if IN_ALLOC {
+        if IN_ALLOC.with(|x| {
+            let mut r = x.borrow_mut();
+            let v = *r;
+            *r = true;
+            v
+        }) {
             eprintln("Recursive allocation (alloc) occured, aborting!");
             process::abort();
         }
-        IN_ALLOC = true;
+        let guard = self.0.lock().unwrap();// not held by the current thread (see prev check)
         let mut buf = [0u8; 20];
         if DO_DEBUG_PRINTS {
             print("allocating ");
             print(layout.size().numtoa_str(10, &mut buf));
             println(" bytes");
         }
-        let mut allocator = RAlloc::from_raw(NonNull::new_unchecked(addr_of_mut!(MEMORY)));
+        let mut allocator = RAlloc::from_raw(guard.map);
         let res = match allocator.allocator_compatable_malloc(layout) {
             Ok(ptr) => ptr,
             Err(..) => {
@@ -80,30 +93,70 @@ unsafe impl GlobalAlloc for GlobalAllocator {
             print(", got an allocation with an offset to that alignment of ");
             println((res as usize % layout.align()).numtoa_str(10, buf.as_mut_slice()));
         }
-        IN_ALLOC = false;
+        drop(guard);
+        IN_ALLOC.with(|x| *x.borrow_mut() = false);
         res
     }
+
     unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
-        if IN_ALLOC {
+        if IN_ALLOC.with(|x| {
+            let mut r = x.borrow_mut();
+            let v = *r;
+            *r = true;
+            v
+        }) {
             eprintln("Recursive allocation (dealloc) occured, aborting!");
             process::abort();
         }
-        IN_ALLOC = true;
+        let guard = self.0.lock().unwrap();// not held by the current thread (see prev check)
         if DO_DEBUG_PRINTS {
             println("deallocating");
         }
-        let mut allocator = RAlloc::from_raw(NonNull::new_unchecked(addr_of_mut!(MEMORY)));
+        ptr.write_bytes(0b10101010u8, layout.size());
+        let mut allocator = RAlloc::from_raw(guard.map);
         allocator.allocator_compatable_free(NonNull::new_unchecked(ptr), layout);
-        IN_ALLOC = false;
-    }
-}
-struct RawGlobalAllocator { }
-impl RawGlobalAllocator {
-    pub const fn new() -> Self {
-        Self { }
+        drop(guard);
+        IN_ALLOC.with(|x| *x.borrow_mut() = false);
     }
 }
 
+pub struct RawGlobalRalloc {
+    map: NonNull<[u8]>
+}
+
+impl RawGlobalRalloc {
+    pub fn new() -> Self {
+        // use std::{fs::File, os::unix::prelude::FromRawFd, ptr::addr_of_mut};
+        // println("Creating alloc");
+        // let file = unsafe {
+        //     let ptr = libc::fopen(b"mem.mmap\0".as_ptr() as _, b"rb+\0".as_ptr() as _);
+        //     if ptr.is_null() {
+        //         eprintln("Error opening file!");
+        //         Err::<(), _>(std::io::Error::last_os_error()).unwrap();
+        //     }
+        //     let desc = libc::fileno(ptr);
+        //     let file = File::from_raw_fd(desc);
+        //     file.set_len(5_000).unwrap();
+        //     file
+        // };
+        // let map = unsafe { memmap2::MmapOptions::new().map_mut(&file) }.unwrap().as_mut().into();
+        // println("memory mapped");
+
+        static mut MEMORY: [u8; 5_000] = [0u8; 5_000];
+        let map = unsafe { MEMORY.as_mut_slice() }.into();
+
+        if unsafe { RAlloc::new(map).is_none() } {
+            eprintln!("Could not create allocator (memory is invalid / too small)");
+            process::abort();
+        }
+
+        Self {
+            map
+        }
+    }
+}
+
+unsafe impl Send for RawGlobalRalloc {} // MmapMut is Send, and it just contains a raw pointer
 
 fn main() {
     println!("Hello, World!");
